bc6121e84988051ef6680b4c5b9ef32ba575c43c
AntonioRadessich <antonio.radessich@studenti.unitn.it>
2020-11-11 16:28:53 +0100

    moduli node, front end base, bug fix sulla get di login

---
+++ static/script.js
@@ -0,0 +1,24 @@
+var loggedUser;
+
+function login()
+{
+    //get the form object
+    var email = document.getElementById("loginEmail").value;
+    //console.log(email);
+
+    fetch('../api/v1?email=' + email)
+    .then((resp) => resp.json()) // Transform the data into json
+    .then(function(data) { // Here you get the data to modify as you please
+        //console.log(data);
+        loggedUser = data[0];
+        console.log(loggedUser);
+        //loggedUser.id = loggedUser.self.substring(loggedUser.self.lastIndexOf('/') + 1);
+        //document.getElementById("loggedUser").innerHTML = loggedUser.email;
+        return;
+    })
+    .catch( error => console.error(error) ); // If there is any error you will catch them here
+
+
+
+    
+};
