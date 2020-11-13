3ddaa523c379ed78c8e8752295c1ea40617c0c4a
Alex Dal Ri <alex.dalri@studenti.unitn.it>
2020-11-13 16:55:46 +0100

    Aggiunto il logout tramite bottone. Aggiunta pagina html di manager

--- static/script.js
+++ static/script.js
@@ -1,5 +1,3 @@
-var loggedUser;
-
 function login()
 {
     //get the form object
@@ -9,16 +7,23 @@ function login()
     fetch('../api/v1?email=' + email)
     .then((resp) => resp.json()) // Transform the data into json
     .then(function(data) { // Here you get the data to modify as you please
-        //console.log(data);
-        loggedUser = data[0];
-        console.log(loggedUser);
-        //loggedUser.id = loggedUser.self.substring(loggedUser.self.lastIndexOf('/') + 1);
-        //document.getElementById("loggedUser").innerHTML = loggedUser.email;
+
+        document.cookie='loggedUser:'+data[0].email;
         return;
     })
     .catch( error => console.error(error) ); // If there is any error you will catch them here
 
 
-
-    
 };
+
+function getUser(){
+    var logged=document.cookie
+    .split("; ")
+    .find(row=>row.startsWith("loggedUser"))
+    .split(":")[1];
+    document.getElementById("btn").innerHTML=logged;
+}
+function logout(){
+    document.cookie="loggedUser:";
+    window.location.href="http://localhost:8000/";
+}
