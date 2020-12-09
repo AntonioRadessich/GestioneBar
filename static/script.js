8937fc38907bc789c617c930f700058d9ae2ff9f
AntonioRadessich <antonio.radessich@studenti.unitn.it>
2020-12-09 17:27:15 +0100

    riviste alcune API, aggiunti casi di errore alle API

--- static/script.js
+++ static/script.js
@@ -4,19 +4,20 @@ function login()
 {
     //get the form object
     var email = document.getElementById("loginEmail").value;
-
     fetch('../api/v1?email=' + email)
     .then((resp) => resp.json()) // Transform the data into json
     .then(function(data) { // Here you get the data to modify as you please
         //document.cookie='loggedUser:'+data[0].email;
-        var category=data.category;
-        loggedUser.token=data.token;
-        loggedUser.category=data.category;
-        loggedUser.self=data.self;
-        loggedUser.email=data.email;
-        localStorage.setItem("loggedUser",JSON.stringify(loggedUser));
-        //alert(JSON.parse(localStorage.getItem("loggedUser")).token);
-        window.location.replace("/"+category+".html",true);
+        if(!(data.email == undefined)){
+            var category=data.category;
+            loggedUser.token=data.token;
+            loggedUser.category=data.category;
+            loggedUser.self=data.self;
+            loggedUser.email=data.email;
+            localStorage.setItem("loggedUser",JSON.stringify(loggedUser));
+            //alert(JSON.parse(localStorage.getItem("loggedUser")).token);
+            window.location.replace("/"+category+".html",true);
+        }
         return false;
     })
     .catch( error => console.error(error) ); // If there is any error you will catch them here
